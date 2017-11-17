de7d3023e935df8c92697bc2a3421fc4b1994711
Mark Voorberg <mark@binaryops.ca>
2017-11-16 22:17:18 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+const ServerFxHandler = require('./lib/serverFxHandler');
+
+function TransomServerFx() {
+// 	this.initialize = function (server, options) {
+// 		server.registry.set(options.registryKey || 'transomServerFunctions', new ServerFxHandler(server, options));
+// 	}
+// }
+
+// module.exports = new TransomServerFx();
+
+// 'use strict';
+// const ServerFxHandler = require('./serverFxHandler');
+// const assert = require('assert');
+
+// function TransomServerFunctions() {
+
+	this.initialize = function (server, options) {
+		const postMiddleware = options.postMiddleware || [];
+		const preMiddleware = [function (req, res, next) {
+			// Delayed resolution of the isLoggedIn middleware.
+			return server.registry.get('isLoggedIn')(req, res, next);
+		}, ...(options.preMiddleware || [])];
+
+		createFunctions(server, preMiddleware, postMiddleware);
+	}
+
+	function createFunctions(server, preMiddleware, postMiddleware) {
+		const serverFxHandler = new ServerFxHandler();
+		const serverFunctions = server.registry.get('transom-options.api_definition.functions', {});
+
+		Object.keys(serverFunctions).map(function (key) {
+			const fx = serverFunctions[key];
+			fx.methods = fx.methods || ['post'];
+
+			const fxPreMiddleware = serverFxHandler.serverFunctionMiddleware(fx, preMiddleware);
+			const wrappedFx = serverFxHandler.getWrapper(server, fx);
+
+			fx.methods.forEach(function (method) {
+				const urlPath = `/v1/:__api_code/:__version/fx/${key}`;
+				switch (method.toLowerCase()) {
+					case "post":
+						server.post(urlPath, fxPreMiddleware, wrappedFx, postMiddleware);
+						break;
+					case "delete":
+						server.del(urlPath, fxPreMiddleware, wrappedFx, postMiddleware);
+						break;
+					case "get":
+						server.get(urlPath, fxPreMiddleware, wrappedFx, postMiddleware);
+						break;
+					default:
+						assert(false, `Function method '${method}' is not supported.`);
+				}
+			});
+		});
+	}
+}
+
+module.exports = new TransomServerFx();
---
+++ test/serverFxHandler.spec.js
@@ -0,0 +1,39 @@
+"use strict";
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const ServerFxHandler = require('../lib/serverFxHandler');
+
+describe('ServerFxHandler', function () {
+
+    beforeEach(function () {
+        // Stub out a mock nodemailer
+        // sinon.stub(nodemailer, 'createTransport').callsFake(function (opts) {
+        //     // whatever you would like innerLib.toCrazyCrap to do under test
+        //     function Transport() {
+        //         this.sendMail = function (opts, cb) {
+        //             if (cb) {
+        //                 return cb(opts.error, opts.response);
+        //             }
+        //         };
+        //         this.close = sinon.spy();
+        //     };
+        //     return new Transport();
+        // });
+    });
+
+    afterEach(function () {
+        // restore original functionality
+        // nodemailer.createTransport.restore();
+    });
+    
+    it('can create a new ServerFxHandler', function () {
+        const dummyServer = {};
+        const dummyOptions = {};
+        const serverFxHandler = new ServerFxHandler(dummyServer, dummyOptions);
+
+        expect(serverFxHandler).to.be.an.instanceof(Object);
+    });
+
+
+
+});
