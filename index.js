1c3419dd758b7ab0916ccc164143f7491322437e
Mark Voorberg <mark@binaryops.ca>
2018-03-09 23:21:23 -0800

    ported hasGroups  to localUser

--- index.js
+++ index.js
@@ -1,75 +1,31 @@
 'use strict';
 const ServerFxHandler = require('./lib/serverFxHandler');
+const debug = require('debug')('transom:serverfunctions');
+const assert = require('assert');
 
 function TransomServerFx() {
-	// 	this.initialize = function (server, options) {
-	// 		server.registry.set(options.registryKey || 'transomServerFunctions', new ServerFxHandler(server, options));
-	// 	}
-	// }
-
-	// module.exports = new TransomServerFx();
-
-	// 'use strict';
-	// const ServerFxHandler = require('./serverFxHandler');
-	// const assert = require('assert');
-
-	// function TransomServerFunctions() {
 
 	this.initialize = function (server, options) {
 		const postMiddleware = options.postMiddleware || [];
-		const preMiddleware = [function (req, res, next) {
-			// Delayed resolution of the isLoggedIn middleware.
-			if (server.registry.has('isLoggedIn')) {
-				server.registry.get('isLoggedIn')(req, res, next);
-			} else {
-				next();
-			}
-		}, ...(options.preMiddleware || [])];
+		const preMiddleware = options.preMiddleware || [];
 
-		createFunctions(server, preMiddleware, postMiddleware);
-	}
-
-	this.preStart = function (server, options) {
+		const serverFxHandler = new ServerFxHandler({server});
 		const serverFunctions = server.registry.get('transom-config.definition.functions', {});
-
-		// Lastly, make sure that the groups referenced in the acl properties are seeded in the security plugin
-		if (server.registry.has('transomLocalUserClient')) {
-			// Collect the distinct groups first
-			// Create Mongoose models from the API definition.
-			const distinctGroups = {};
-			Object.keys(serverFunctions).forEach(function (key) {
-				const acl = serverFunctions[key].acl || {};
-				if (acl.groups) {
-					if (typeof acl.groups === 'string') {
-						acl.groups = [acl.groups];
-					}
-					acl.groups.map(function (group) {
-						// Build a list of distinct group codes.
-						group = group.toLowerCase().trim();
-						distinctGroups[group] = true;
-					});
-				}
-			});
-			const localUserClient = server.registry.get('transomLocalUserClient');
-			localUserClient.setGroups(server, Object.keys(distinctGroups));
-		}
-	}
-
-	function createFunctions(server, preMiddleware, postMiddleware) {
-		const serverFxHandler = new ServerFxHandler();
-		const serverFunctions = server.registry.get('transom-config.definition.functions', {});
+		const uriPrefix = server.registry.get('transom-config.definition.uri.prefix');
 
 		Object.keys(serverFunctions).map(function (key) {
 			const fx = serverFunctions[key];
 			fx.methods = fx.methods || ['post'];
 
+			// Add Authentication and Authorization middleware from localUserMiddleware.
 			const fxPreMiddleware = serverFxHandler.serverFunctionMiddleware(fx, preMiddleware);
-			const wrappedFx = serverFxHandler.getWrapper(server, fx);
 
-			const uriPrefix = server.registry.get('transom-config.definition.uri.prefix');
+			// Use a wrapper to inject 'server' into the request handler function.
+			const wrappedFx = serverFxHandler.getWrapper(fx);
+			const urlPath = `${uriPrefix}/fx/${key}`;
 
 			fx.methods.forEach(function (method) {
-				const urlPath = `${uriPrefix}/fx/${key}`;
+				debug(`TransomServerFunctions adding route: (${method}) ${urlPath}`);
 				switch (method.toLowerCase()) {
 					case "post":
 						server.post(urlPath, fxPreMiddleware, wrappedFx, postMiddleware);
@@ -81,10 +37,42 @@ function TransomServerFx() {
 						server.get(urlPath, fxPreMiddleware, wrappedFx, postMiddleware);
 						break;
 					default:
-						assert(false, `Function method '${method}' is not supported.`);
+					debug(`TransomServerFunction method '${method}' is not supported.`);
+					assert(false, `TransomServerFunction method '${method}' is not supported.`);
 				}
 			});
 		});
+	}
+
+	this.preStart = function (server, options) {
+		const serverFunctions = server.registry.get('transom-config.definition.functions', {});
+
+		// Lastly, make sure that the groups referenced in the acl properties are seeded in the security plugin.
+		if (server.registry.has('transomLocalUserClient')) {
+			const localUserClient = server.registry.get('transomLocalUserClient');
+
+			// Collect a complete list of all the Acl group codes.
+			const groups = [];
+			Object.keys(serverFunctions).forEach(function (key) {
+				const acl = serverFunctions[key].acl || {};
+				if (acl.groups) {
+					if (typeof acl.create === 'string') {
+						acl.groups = [acl.groups];
+					}
+					groups.push(...acl.groups);
+				}
+			});
+
+			// Build a list of distinct group codes.
+			const distinctGroups = {};
+			groups.map(function (group) {
+				group = group.toLowerCase().trim();
+				distinctGroups[group] = true;
+			});
+
+			debug(`TransomServerFunctions seeding groups [${groups.join(',')}]`);
+			localUserClient.setGroups(server, distinctGroups);
+		}
 	}
 }
 
